9b6d642a43a85be8e216310de4d792ff8e8101cb
root <root@135-181-137-214.ptr>
2024-04-19 23:41:26 +0200

    remove count code segment and make file

--- Makefile
+++ Makefile
@@ -9,20 +9,25 @@ optimize:
 test:
 	cargo unit-test
 
-FROM=nibi10rdtquh3jl44hg00x0plzeawuclqqet0he4692
+WALLET=nibi1d3lmwkgjgdyfpsn4xgh299jnpk4r89kd5xs420
 AIRDROP_CONTRACT=nibi178kzznh9cepjckjefqc3mgt9gf9rfkyw6kk0pymeypx9rplggvyq9yjjuv
+CODE_ID=421
+WALLET_NAME=jarvis
 
 make-wallet:
-	@nibid keys add wallet
+	@nibid keys add wallet ${WALLET_NAME}
 
 show-wallet:
-	@nibid keys show -a ${id}
+	@nibid keys show -a ${WALLET}
+
+get-balance:
+	@nibid query bank balances ${WALLET} --denom unibi
 
 upload-testnet:
-	@nibid tx wasm store artifacts/jarvis_airdrop.wasm --from ${FROM} --gas auto --gas-adjustment 1.5 --gas-prices 0.025unibi --yes
+	@nibid tx wasm store artifacts/jarvis_airdrop.wasm --from ${WALLET} --gas auto --gas-adjustment 1.5 --gas-prices 0.025unibi --yes
 
 instantiate-testnet:
-	@nibid tx wasm instantiate ${id} '{"count": 1}' --admin ${FROM} --label airdrop --from ${FROM} --gas auto --gas-adjustment 1.5 --gas-prices 0.025unibi --yes
+	@nibid tx wasm instantiate ${CODE_ID} '{"count": 1}' --admin ${WALLET} --label airdrop --from ${WALLET} --gas auto --gas-adjustment 1.5 --gas-prices 0.025unibi --yes
 
 get-count:
 	$(eval GET_COUNT := $$(shell cat ./commands/get_count.json))
@@ -38,8 +43,8 @@ get-all-nfts:
 
 exe-set-nft-contract-addr:
 	$(eval SET_NFT_CONTRACT_ADDR := $$(shell cat ./commands/set_nft_contract_addr.json))
-	@nibid tx wasm execute ${AIRDROP_CONTRACT} '$(SET_NFT_CONTRACT_ADDR)' --from ${FROM} --gas auto --gas-adjustment 1.5 --gas-prices 0.025unibi --yes 
+	@nibid tx wasm execute ${AIRDROP_CONTRACT} '$(SET_NFT_CONTRACT_ADDR)' --from ${WALLET} --gas auto --gas-adjustment 1.5 --gas-prices 0.025unibi --yes 
 
 exe-send-nfts:
 	$(eval SEND_NFTS := $$(shell cat ./commands/send_nfts.json))
-	@nibid tx wasm execute ${AIRDROP_CONTRACT} '$(SEND_NFTS)' --from ${FROM} --gas auto --gas-adjustment 1.5 --gas-prices 0.025unibi --yes
+	@nibid tx wasm execute ${AIRDROP_CONTRACT} '$(SEND_NFTS)' --from ${WALLET} --gas auto --gas-adjustment 1.5 --gas-prices 0.025unibi --yes
--- src/contract.rs
+++ src/contract.rs
@@ -1,12 +1,15 @@
 #[cfg(not(feature = "library"))]
 use cosmwasm_std::entry_point;
-use cosmwasm_std::{to_json_binary, Binary, Deps, DepsMut, Env, MessageInfo, Response, StdResult, Addr, CosmosMsg, WasmMsg, SubMsg};
+use cosmwasm_std::{
+    to_json_binary, Addr, Binary, CosmosMsg, Deps, DepsMut, Env, MessageInfo, Response, StdResult,
+    SubMsg, WasmMsg,
+};
 use cw2::set_contract_version;
 use cw721::Cw721ExecuteMsg;
 
 use crate::error::ContractError;
-use crate::msg::{ExecuteMsg, GetCountResponse, InstantiateMsg, QueryMsg, AllNftsResponse, NftContractAddrResponse};
-use crate::state::{State, STATE, NFTS, NFT_CONTRACT_ADDR};
+use crate::msg::{AllNftsResponse, ExecuteMsg, InstantiateMsg, NftContractAddrResponse, QueryMsg};
+use crate::state::{State, NFTS, NFT_CONTRACT_ADDR, STATE};
 
 // version info for migration info
 const CONTRACT_NAME: &str = "crates.io:jarvis-airdrop";
@@ -20,7 +23,6 @@ pub fn instantiate(
     msg: InstantiateMsg,
 ) -> Result<Response, ContractError> {
     let state = State {
-        count: msg.count,
         owner: info.sender.clone(),
     };
     set_contract_version(deps.storage, CONTRACT_NAME, CONTRACT_VERSION)?;
@@ -28,8 +30,7 @@ pub fn instantiate(
 
     Ok(Response::new()
         .add_attribute("method", "instantiate")
-        .add_attribute("owner", info.sender)
-        .add_attribute("count", msg.count.to_string()))
+        .add_attribute("owner", info.sender))
 }
 
 #[cfg_attr(not(feature = "library"), entry_point)]
@@ -40,36 +41,20 @@ pub fn execute(
     msg: ExecuteMsg,
 ) -> Result<Response, ContractError> {
     match msg {
-        ExecuteMsg::Increment {} => execute::increment(deps),
-        ExecuteMsg::Reset { count } => execute::reset(deps, info, count),
-        ExecuteMsg::SetNftContractAddr { addr } => execute::set_nft_contract_addr(deps, env, info, addr),
-        ExecuteMsg::ReceiveNft { sender, token_id, msg } => execute::receive_nft(deps, env, info, token_id),
-        ExecuteMsg::SendNfts { allocations } => execute::send_nfts(deps, env, info, allocations), 
+        ExecuteMsg::SetNftContractAddr { addr } => {
+            execute::set_nft_contract_addr(deps, env, info, addr)
+        }
+        ExecuteMsg::ReceiveNft {
+            sender,
+            token_id,
+            msg,
+        } => execute::receive_nft(deps, env, info, token_id),
+        ExecuteMsg::SendNfts { allocations } => execute::send_nfts(deps, env, info, allocations),
     }
 }
 
 pub mod execute {
     use super::*;
-
-    pub fn increment(deps: DepsMut) -> Result<Response, ContractError> {
-        STATE.update(deps.storage, |mut state| -> Result<_, ContractError> {
-            state.count += 1;
-            Ok(state)
-        })?;
-
-        Ok(Response::new().add_attribute("action", "increment"))
-    }
-
-    pub fn reset(deps: DepsMut, info: MessageInfo, count: i32) -> Result<Response, ContractError> {
-        STATE.update(deps.storage, |mut state| -> Result<_, ContractError> {
-            if info.sender != state.owner {
-                return Err(ContractError::Unauthorized {});
-            }
-            state.count = count;
-            Ok(state)
-        })?;
-        Ok(Response::new().add_attribute("action", "reset"))
-    }
 
     pub fn set_nft_contract_addr(
         deps: DepsMut,
@@ -87,7 +72,9 @@ pub mod execute {
         // Optionally, add authorization checks here to ensure only specific addresses can update this
         let nft_contract_addr = deps.api.addr_validate(&addr)?;
         NFT_CONTRACT_ADDR.save(deps.storage, &nft_contract_addr)?;
-        Ok(Response::new().add_attribute("action", "set_nft_contract_addr").add_attribute("address", addr))
+        Ok(Response::new()
+            .add_attribute("action", "set_nft_contract_addr")
+            .add_attribute("address", addr))
     }
 
     pub fn receive_nft(
@@ -99,10 +86,9 @@ pub mod execute {
         let mut nfts = NFTS.load(deps.storage).unwrap_or_default();
         nfts.push(token_id);
         NFTS.save(deps.storage, &nfts)?;
-    
+
         Ok(Response::new().add_attribute("action", "receive_nft"))
     }
-    
 
     pub fn send_nfts(
         deps: DepsMut,
@@ -123,7 +109,7 @@ pub mod execute {
 
         let mut nfts = NFTS.load(deps.storage)?;
         let mut response = Response::new().add_attribute("action", "send_nfts");
-    
+
         for (recipient, amount) in allocations {
             for _ in 0..amount {
                 if let Some(token_id) = nfts.pop() {
@@ -132,20 +118,20 @@ pub mod execute {
                         recipient: recipient.to_string(),
                         token_id: token_id,
                     };
-        
+
                     let msg = CosmosMsg::Wasm(WasmMsg::Execute {
                         contract_addr: validated_addr.clone().to_string(),
                         msg: to_json_binary(&transfer_msg)?,
                         funds: vec![],
                     });
-                    
+
                     response.messages.push(SubMsg::new(msg));
                 } else {
                     return Err(ContractError::InsufficientNFTs {});
                 }
             }
         }
-    
+
         NFTS.save(deps.storage, &nfts)?;
         Ok(response)
     }
@@ -154,25 +140,19 @@ pub mod execute {
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn query(deps: Deps, _env: Env, msg: QueryMsg) -> StdResult<Binary> {
     match msg {
-        QueryMsg::GetCount {} => to_json_binary(&query::count(deps)?),
-        QueryMsg::GetAllNfts {  } => to_json_binary(&query::all_nfts(deps)?),
-        QueryMsg::GetNftContractAddr {  } => to_json_binary(&query::nft_contract_addr(deps)?),
+        QueryMsg::GetAllNfts {} => to_json_binary(&query::all_nfts(deps)?),
+        QueryMsg::GetNftContractAddr {} => to_json_binary(&query::nft_contract_addr(deps)?),
     }
 }
 
 pub mod query {
     use super::*;
 
-    pub fn count(deps: Deps) -> StdResult<GetCountResponse> {
-        let state = STATE.load(deps.storage)?;
-        Ok(GetCountResponse { count: state.count })
-    }
-
     pub fn all_nfts(deps: Deps) -> StdResult<AllNftsResponse> {
         let nfts = NFTS.load(deps.storage)?;
         Ok(AllNftsResponse { nfts })
     }
-    
+
     pub fn nft_contract_addr(deps: Deps) -> StdResult<NftContractAddrResponse> {
         let nft_contract_addr = NFT_CONTRACT_ADDR.load(deps.storage)?;
         Ok(NftContractAddrResponse { nft_contract_addr })
@@ -189,63 +169,11 @@ mod tests {
     fn proper_initialization() {
         let mut deps = mock_dependencies();
 
-        let msg = InstantiateMsg { count: 17 };
+        let msg = InstantiateMsg {};
         let info = mock_info("creator", &coins(1000, "earth"));
 
         // we can just call .unwrap() to assert this was a success
         let res = instantiate(deps.as_mut(), mock_env(), info, msg).unwrap();
         assert_eq!(0, res.messages.len());
-
-        // it worked, let's query the state
-        let res = query(deps.as_ref(), mock_env(), QueryMsg::GetCount {}).unwrap();
-        let value: GetCountResponse = from_json(&res).unwrap();
-        assert_eq!(17, value.count);
-    }
-
-    #[test]
-    fn increment() {
-        let mut deps = mock_dependencies();
-
-        let msg = InstantiateMsg { count: 17 };
-        let info = mock_info("creator", &coins(2, "token"));
-        let _res = instantiate(deps.as_mut(), mock_env(), info, msg).unwrap();
-
-        // beneficiary can release it
-        let info = mock_info("anyone", &coins(2, "token"));
-        let msg = ExecuteMsg::Increment {};
-        let _res = execute(deps.as_mut(), mock_env(), info, msg).unwrap();
-
-        // should increase counter by 1
-        let res = query(deps.as_ref(), mock_env(), QueryMsg::GetCount {}).unwrap();
-        let value: GetCountResponse = from_json(&res).unwrap();
-        assert_eq!(18, value.count);
-    }
-
-    #[test]
-    fn reset() {
-        let mut deps = mock_dependencies();
-
-        let msg = InstantiateMsg { count: 17 };
-        let info = mock_info("creator", &coins(2, "token"));
-        let _res = instantiate(deps.as_mut(), mock_env(), info, msg).unwrap();
-
-        // beneficiary can release it
-        let unauth_info = mock_info("anyone", &coins(2, "token"));
-        let msg = ExecuteMsg::Reset { count: 5 };
-        let res = execute(deps.as_mut(), mock_env(), unauth_info, msg);
-        match res {
-            Err(ContractError::Unauthorized {}) => {}
-            _ => panic!("Must return unauthorized error"),
-        }
-
-        // only the original creator can reset the counter
-        let auth_info = mock_info("creator", &coins(2, "token"));
-        let msg = ExecuteMsg::Reset { count: 5 };
-        let _res = execute(deps.as_mut(), mock_env(), auth_info, msg).unwrap();
-
-        // should now be 5
-        let res = query(deps.as_ref(), mock_env(), QueryMsg::GetCount {}).unwrap();
-        let value: GetCountResponse = from_json(&res).unwrap();
-        assert_eq!(5, value.count);
     }
 }
